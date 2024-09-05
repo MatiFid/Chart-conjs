d716ae35f2b5d0659e205adef86337263f11f24f
MatiFid <matias10242@hotmail.com>
2024-09-04 21:23:05 -0300

    Update get_weather_data.py

--- get_weather_data.py
+++ get_weather_data.py
@@ -7,22 +7,22 @@ def fetch_weather_data():
     lat = "32.4072"  # Latitud de Villa Maria, por ejemplo
     lon = "-63.2403"  # Longitud de Villa Maria, por ejemplo
     
-    # URL que incluye tanto los datos meteorológicos como la calidad del aire
+    # URL para datos meteorológicos
     weather_url = f"https://api.weatherbit.io/v2.0/current?lat={lat}&lon={lon}&key={api_key}&units=M"
 
     try:
-        # Obtener los datos meteorológicos y de calidad del aire en una sola solicitud
+        # Obtener los datos meteorológicos
         weather_response = requests.get(weather_url)
         weather_data = weather_response.json()
 
-        print("Datos obtenidos con éxito.")
+        print("Datos meteorológicos obtenidos con éxito.")
     except Exception as e:
         print(f"Error al obtener los datos: {e}")
         return
 
     current_weather = weather_data['data'][0]
 
-    # Combinar datos meteorológicos y calidad del aire
+    # Datos meteorológicos sin la calidad del aire
     combined_data = {
         "temperature": current_weather['temp'],
         "humidity": current_weather['rh'],
@@ -30,14 +30,6 @@ def fetch_weather_data():
         "wind_speed": current_weather['wind_spd'],
         "apparent_temperature": current_weather['app_temp'],
         "cloud_coverage": current_weather['clouds'],
-        "air_quality": {
-            "pm10": current_weather['pm10'],
-            "pm2_5": current_weather['pm2_5'],
-            "o3": current_weather['o3'],
-            "no2": current_weather['no2'],
-            "so2": current_weather['so2'],
-            "co": current_weather['co'],
-        },
         "uv_index": current_weather['uv']
     }
 
@@ -49,7 +41,7 @@ def fetch_weather_data():
     # Guardar los datos en el archivo JSON
     with open('weather_data.json', 'w') as json_file:
         json.dump(combined_data, json_file, indent=4)
-        print("Datos guardados exitosamente en weather_data.json.")
+        print("Datos meteorológicos guardados exitosamente en weather_data.json.")
 
 if __name__ == "__main__":
     fetch_weather_data()
